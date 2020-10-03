3378d9f3cc290d0497ed0f3e881a4a10c7c8ed31
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-03 09:28:36 -0400

    add progress messages

--- import_gpx.sh
+++ import_gpx.sh
@@ -10,7 +10,14 @@ if [ -f "$1" ]; then
 	exit 1
 fi
 
+echo import gpx to spatialite database
 ogr2ogr -f SQLite -dim XYZ -lco GEOMETRY_NAME=geom -t_srs EPSG:26986 -dsco SPATIALITE=yes "$1" track_points.gpx track_points
+
+echo create track_lines table
 spatialite -batch -silent "$1" < create_track_lines_table.sql
+
+echo add geometry column
 spatialite -batch -silent "$1" < create_geometry_column.sql
+
+echo convert point data to lines
 spatialite -batch -silent "$1" < convert_to_lines.sql
